4eb463fde8c39d764bfa30cab035c36fd6a33ada
Complexlity <nwalozielijah@gmail.com>
2025-05-14 19:40:00 +0100

    chore: replace wallet ui with solana wallet adapter

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,13 +1,15 @@
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ReactQueryProvider } from '../providers/react-query-provider'
-import { SolanaProvider } from '@/components/providers/solana-provider'
 import React from 'react'
+import AppWalletProvider from './wallet-provider'
 
 export function AppProviders({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ReactQueryProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        <SolanaProvider>{children}</SolanaProvider>
+        <AppWalletProvider>
+      {children}
+        </AppWalletProvider>
       </ThemeProvider>
     </ReactQueryProvider>
   )
