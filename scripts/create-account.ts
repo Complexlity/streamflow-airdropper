707116704262880387813f4768666dbc914c9263
Complexlity <nwalozielijah@gmail.com>
2025-05-14 14:53:47 +0100

    chore: update script, installs

--- scripts/create-account.ts
+++ scripts/create-account.ts
@@ -1,14 +1,15 @@
-import { Connection, Keypair } from '@solana/web3.js';
+import { clusterApiUrl, Connection, Keypair } from '@solana/web3.js';
+import { ICluster } from '@streamflow/common';
 import fs from 'fs';
-import { config } from 'dotenv'
 
-config()
-
-const devnetRpcUrl = 'https://api.devnet.solana.com'
-const connection = new Connection(devnetRpcUrl)
+const connection = new Connection(clusterApiUrl(ICluster.Devnet), "confirmed")
 
 
 const keypair = Keypair.generate();
+//Log the account address
+console.log('Keypair generated:', keypair.publicKey.toBase58());
+
+
 const secretKey = Array.from(keypair.secretKey); // Convert Uint8Array to array of numbers
 fs.writeFileSync('keypair.json', JSON.stringify(secretKey)); // Save as JSON array
 
