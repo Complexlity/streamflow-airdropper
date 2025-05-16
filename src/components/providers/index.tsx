2279bfff437871cba23db14f233eaf0d9b3bdd32
Complexlity <nwalozielijah@gmail.com>
2025-05-16 19:44:28 +0100

    chore: rename provider files

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,14 +1,14 @@
-import { ThemeProvider } from '@/components/providers/theme-provider'
-import { ReactQueryProvider } from '../providers/react-query-provider'
+import { ThemeProvider } from '@/components/providers/ThemeProvider'
+import { ReactQueryProvider } from './ReactQueryProvider'
 import React from 'react'
-import AppWalletProvider from './wallet-provider'
+import AppWalletProvider from './WalletProvider'
 
 export function AppProviders({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ReactQueryProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         <AppWalletProvider>
-      {children}
+          {children}
         </AppWalletProvider>
       </ThemeProvider>
     </ReactQueryProvider>
