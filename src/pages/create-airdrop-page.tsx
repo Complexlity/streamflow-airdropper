6abb744a231c924f5026332361bd4fba9aec069d
Complexlity <nwalozielijah@gmail.com>
2025-05-16 21:40:09 +0100

    chore: clean code

--- src/pages/create-airdrop-page.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateAirdropForm } from "@/components/airdrop/create-form"
-
-export function CreateAirdropPage() {
-  return (
-    <div className="container mx-auto py-8">
-      <CreateAirdropForm />
-    </div>
-  )
-}
